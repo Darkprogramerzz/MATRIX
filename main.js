a7e88559139a372814b16bc7196e886a7baefc74
Darkprogrammer <war20101130@gmail.com>
2024-01-16 12:50:40 +0530

    ds

--- main.js
+++ main.js
@@ -31,6 +31,18 @@ WinDow.addEventListener("scroll", function() {
     let navbar =document.querySelector('.NavBar')
     navbar.classList.toggle('navbar',WinDow.scrollTop > 500)
     if(WinDow.scrollTop > 500){
-        document.getElementById("navlogo").src = "hackanm.gif";
+        document.getElementById("navlogo").src ="assets/navlogo1.png";
+        document.getElementById("navlogo").style.width = '120px';
+        document.getElementById("navlogo").style.height = '80px';
+        document.getElementById("navlogo").style.marginRight = '0px';
+        document.getElementById("NavHeading").style.color = 'var(--Pink)';
+        document.getElementById("NavHeading").style.textShadow ='5px 5px var(--LightBlue)';
+    }
+    if(WinDow.scrollTop < 500){
+        document.getElementById("navlogo").src ="assets/Capture(1).png";
+        document.getElementById("navlogo").style.width = '75px';
+        document.getElementById("navlogo").style.height = '50px';
+        document.getElementById("navlogo").style.marginRight = '15px';
+        document.getElementById("NavHeading").style.color = 'var(--Yellow)';
     }
   });
